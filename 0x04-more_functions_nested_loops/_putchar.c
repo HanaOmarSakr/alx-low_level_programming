ce5134409cf582a891394be8dba6d33fff7b293c
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-06-23 20:01:15 -0700

    t0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar writes characters
+ * @c : the char to be printed
+ * Return: on success 1
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
