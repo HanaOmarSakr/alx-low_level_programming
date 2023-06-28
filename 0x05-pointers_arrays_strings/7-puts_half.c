9deaae79178022c4e04121f4d6ef6e1fbdc5c9b9
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-06-27 18:39:55 -0700

    t

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,14 +10,24 @@
 void puts_half(char *str)
 {
 	int i = 0;
+	int j;
 
 	while (str[i] != '\0')
 	{
 		i++;
 	}
-	for (i /= 2; str[i] != '\0'; i++)
+	if (i % 2 == 0)
+	{
+		j = i / 2;
+	}
+	else
+	{
+		j = (i - 1) / 2;
+	}
+	while (j <= i)
 	{
 		_putchar(str[i]);
+		j++;
 	}
 	_putchar('\n');
 }
