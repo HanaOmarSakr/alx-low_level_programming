ab781fd2072265d8500f9ab7832b6e5e6ba10028
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-06-27 17:30:09 -0700

    t

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,16 +9,14 @@
 void print_rev(char *s)
 {
 	int i = 0;
-	int c;
 
-	while (s[i] != '\0')
+	while (s[i])
 	{
 		i++;
 	}
-	while (i > 0)
+	while (i--)
 	{
 		_putchar(s[i]);
-		i--;
 	}
 	_putchar('\n');
 }
