9d0a373cfa4911d348ac186a35f7474d055a835b
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-06-30 15:30:26 -0700

    t

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
-*_strcat - concatenates two strings
-*@dest: the destination string
-*@src: the source string
-*Return: a pointer to the resulting string
-*/
+ * _strcat - function
+ *  @dest: pointer
+ *  @src: pointer
+ *  Return: pointer
+ */
 
 char *_strcat(char *dest, char *src)
 {
