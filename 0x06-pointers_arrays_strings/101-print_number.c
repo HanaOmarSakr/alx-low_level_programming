39a95e2fe2d08b5ee22962593a05425f31339486
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-06-30 17:20:49 -0700

    t

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,24 @@
+/**
+ * print_number - prints an integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	int i;
+	int divisor = 1;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	while (n / divisor >= 10)
+	{
+		divisor *= 10;
+	}
+
+	for (i = divisor; i > 0; i /= 10)
+	{
+		_putchar((n / i) % 10 + '0');
+	}
+}
