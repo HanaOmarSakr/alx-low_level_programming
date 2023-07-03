2dda4983784de7b9ac181913ae6b8c1979e42eb8
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-03 13:44:26 -0700

    task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,4 +1,18 @@
 #include "main.h"
 
 /**
- *
+ * *_memcpy - copies memory
+ * @dest: input
+ * @src: input
+ * @n: input
+ * Return: someting
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	Return(dest);
+}
