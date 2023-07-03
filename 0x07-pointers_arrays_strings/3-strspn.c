e400c16f8bb417ca271217461168e8c0e7181b65
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-03 14:37:44 -0700

    task

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,7 +13,7 @@ unsigned int _strspn(char *s, char *accept)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j]; j++)
+		for (j = 0; j < 100; j++ )
 		{
 			if (accept[j] == '\0')
 				return (i);
