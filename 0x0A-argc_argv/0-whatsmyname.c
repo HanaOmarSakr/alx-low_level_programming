7ae222c8b0ade9fd40514dd5816ac9e2cae973ad
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-07 18:15:20 -0700

    tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of function
+ * @argc: input
+ * @argv: input
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argc;
+	printf ("%s\n" , argv[0]);
+	return (0);
+}
