bee998ef2a6218689fce507ab396a0b7d4e71bf4
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-07 18:27:11 -0700

    tasks

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of function
+ * @argc: input
+ * @argv: input
+ * Return: 0
+ */
+
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
