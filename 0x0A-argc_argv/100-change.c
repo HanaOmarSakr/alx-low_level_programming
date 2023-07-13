16b664d426a9fc494bfad7e684985b987b9cb742
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-12 17:15:27 -0700

    task5

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - prints name of function
+* @argc: input
+* @argv: input
+* Return: 0
+*/
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		printf("Error\n");
+	}
+	int cents = atoi(argv[1]);
+
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	int coins = 0;
+
+	while (cents > 0)
+	{
+		if (cents >= 25)
+		{
+			cents -= 25;
+			coins++;
+		}
+		else if (cents >= 10)
+		{
+			cents -= 10;
+			coins++;
+		}
+		else if (cents >= 5)
+			cents -= 5;
+			coins++;
+		else if (cents >= 2)
+			cents -= 2;
+			coins++;
+		else
+			cents -= 1;
+			coins++;
+	}
+	printf("%d\n", coins);
+	return (0);
+}
+
