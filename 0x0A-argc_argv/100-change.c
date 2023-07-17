dbea6c227b4a47e4bcc88553c2553117be22cb45
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-17 12:36:29 -0700

    task4.0

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,44 +8,30 @@
 */
 int main(int argc, char **argv)
 {
-	if (argc != 2)
+	if (argc == 2)
+	{
+		int cents[] = {25, 10, 5, 2, 1};
+		int i, least = 0;
+		int money = atoi(argv[1]);
+
+		for (i = 0; i < 5; i++)
+		{
+			if (money >= cents[i])
+			{
+				least += money / cents[i];
+				money = money % cents[i];
+				if (money % cents[i] == 0)
+				{
+					break;
+				}
+			}
+		}
+	}
+	printf("%d\n", least);
+	else
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int cents = atoi(argv[1]);
-
-	if (cents < 0)
-	{
-		printf("0\n");
-		return (1);
-	}
-
-	int coins = 0;
-
-	while (cents > 0)
-	{
-		if (cents >= 25)
-		{
-			cents -= 25;
-			coins++;
-		}
-		else if (cents >= 10)
-		{
-			cents -= 10;
-			coins++;
-		}
-		else if (cents >= 5)
-			cents -= 5;
-			coins++;
-		else if (cents >= 2)
-			cents -= 2;
-			coins++;
-		else
-			cents -= 1;
-			coins++;
-	}
-	printf("%d\n", coins);
 	return (0);
 }
-
