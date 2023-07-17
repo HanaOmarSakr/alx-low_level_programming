9ed7fae404aa23a0807dc994e5fd2412dda45b28
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-17 12:17:15 -0700

    task4.0

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,33 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 /**
- * main - prints name of function
- * @argc: input
- * @argv: input
+ * check_num - prints name of function
+ * @str: input
  * Return: 0
  */
 
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+/**
+ * main - prints name of function
+ * @argv: input
+ * @argc: input
+ * Return: 0
+ */
+
+
 int main(int argc, char **argv)
 {
-	int i;
-	int temp;
+	int count;
+	int str_to_int;
 	int sum = 0;
 
-	if (argc == 1)
+	while (count < argc)
 	{
-		printf("0\n");
-		return (2);
-	}
-	for (i = 1; i < argc; i++)
-	{
-		temp = atoi(argv[i]);
-		if (temp == 0)
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum = sum + temp;
+		count++;
 	}
 	printf("%d\n", sum);
 	return (0);
