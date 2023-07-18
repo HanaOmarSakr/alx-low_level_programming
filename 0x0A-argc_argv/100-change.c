60a73dd90ee715e91c0b6e87f2c23d9c757fb784
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-18 05:20:27 -0700

    t100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,37 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
-* main - prints name of function
-* @argc: input
-* @argv: input
-* Return: 0
-*/
-int main(int argc, char **argv)
+ * main - prints the minimum number of coins to make change
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: 0 on success, 1 on error
+ */
+int main(int argc, char *argv[])
 {
-	if (argc == 2)
-	{
-		int cents[] = {25, 10, 5, 2, 1};
-		int i, least = 0;
-		int money = atoi(argv[1]);
+	int cents, coins, i;
+	int values[] = {25, 10, 5, 2, 1}; /* array of coin values */
 
-		for (i = 0; i < 5; i++)
-		{
-			if (money >= cents[i])
-			{
-				least += money / cents[i];
-				money = money % cents[i];
-				if (money % cents[i] == 0)
-				{
-					break;
-				}
-			}
-		}
-	}
-	printf("%d\n", least);
-	else
+	if (argc != 2) /* check if the number of arguments is valid */
 	{
 		printf("Error\n");
 		return (1);
 	}
+
+	cents = atoi(argv[1]); /* convert the argument to an integer */
+	coins = 0;
+
+	if (cents > 0) /* only process positive amounts */
+	{
+		for (i = 0; i < 5; i++) /* loop through the coin values */
+		{
+			coins += cents / values[i]; /* add the number of coins of this value */
+			cents %= values[i]; /* update the remaining cents */
+		}
+	}
+
+	printf("%d\n", coins); /* print the result */
 	return (0);
 }
