fcad96fa69c4b752f2adbb5253d9f709223abb12
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-18 06:14:42 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,43 +8,44 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int len1 = 0;
-	int len2 = 0;
-	char *str;
+        int len1 = 0;
+        int len2 = 0;
+        char *str;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+        if (s1 == NULL)
+                s1 = "";
+        if (s2 == NULL)
+                s2 = "";
 
-	while (s1[len1] != '\0')
-	{
+        while (s1[len1] != '\0')
+        {
+                len1++;
+        }
+        while (s2[len2] != '\0')
+                len2++;
+
+        int t = len1 + len2;
+
+        str = malloc(sizeof(char) * (t + 1));
+
+        if  (str == NULL)
+                return (NULL);
+
+        len1 = len2 = 0;
+        while (s1[len1] != '\0')
+        {
+                str[len1] = s1[len1];
+                len1++;
+        }		str[len1] = s1[len1];
 		len1++;
 	}
 	while (s2[len2] != '\0')
-		len2++;
-
-	int t = len1 + len2;
-
-	str = malloc(sizeof(char) * (t + 1));
-
-	if  (str == NULL)
-		return (NULL);
-
-	len1 = len2 = 0;
-	while (s1[len1] != '\0')
 	{
-		str[len1] = s1[len1];
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		str[len1] = s2[len2];
-		len1++;
+		str[len1 + len2] = s2[len2];
 		len2++;
 	}
 
-	str[len1] = '\0';
+	str[len1 + len2] = '\0';
 	return (str);
 }
 
