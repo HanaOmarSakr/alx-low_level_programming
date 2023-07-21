e0c9dd950d59f46a80526726775279a2d11b7ce3
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-07-21 16:20:46 -0700

    task2

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+/**
+ * array_iterator - prints each
+ * @array: input
+ * @size: input
+ * @action: input
+ * Return: void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
