79f28dea7c9b4dee98ade4462e2c305bd90a5e97
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-08-07 12:32:56 -0700

    task2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - ..
+ * @argc: ...
+ * @argv: ..
+ * Return: ..
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s", argv[i]);
+	}
+	return (0);
+}
