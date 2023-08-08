edbc61cbd197c5af53d4542e1cb9bfb79682f6c4
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-08-08 16:16:24 -0700

    task2

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * argstostr - ..
+ * @ac: ..
+ * @av: ..
+ * Return: ..
+ */
+
+char *argstostr(int ac, char **av)
+{
+	int i;
+	int pos;
+	int len;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	len = 0;
+	for (i = 0; i < ac; i++)
+	{
+		len += strlen(av[i]) + 1;
+	}
+	*str = malloc(len + 1);
+	if (str == NULL)
+		return (NULL);
+
+	pos = 0;
+	for (i = 0; i < ac; i++)
+	{
+		strcpy(str + pos, av[i]);
+		pos += strlen(av[i]);
+		str[pos] = '\n';
+		pos++;
+	}
+	str[pos] = '\0';
+	return (str);
+}
