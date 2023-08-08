723d29f04223c45ef675764c2bb5d38eb588c2dc
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-08-08 07:03:30 -0700

    task2

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+
 /**
  * main - ..
  * @argc: ...
@@ -9,30 +11,25 @@
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	(void)argv;
-
-	int i;
-	int sum = 0;
-	int temp;
-	char *endptr;
-
-	if (argc == 1)
-	{
-		printf("0\n");
-		return (2);
-	}
-
-	for (i = 1; i < argc; i++)
-	{
-		temp = strtol(argv[i], &endptr, 10);
-		if (*endptr != '\0')
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum = sum + temp;
-	}
-	printf("%d\n", sum);
-	return (0);
+    int sum = 0; // initialize the sum to zero
+    for (int i = 1; i < argc; i++) // loop through the command-line arguments
+    {
+        char *arg = argv[i]; // get the current argument
+        for (int j = 0; arg[j] != '\0'; j++) // loop through the characters of the argument
+        {
+            if (!isdigit(arg[j])) // if the character is not a digit
+            {
+                printf("Error\n"); // print error message
+                return 1; // exit with status code 1
+            }
+        }
+        int num = atoi(arg); // convert the argument to an integer
+        if (num > 0) // if the number is positive
+        {
+            sum += num; // add it to the sum
+        }
+    }
+    printf("%d\n", sum); // print the sum, followed by a new line
+    return 0; // exit with status code 0
 }
+
