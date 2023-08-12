9b2eec8904711d4f2517df144fc749ee65fcabba
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-08-11 18:03:18 -0700

    task2

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+int i;
+
+if (argc != 3)
+{
+printf("Error\n");
+exit(98);
+}
+
+for (i = 1; i < argc; i++)
+{
+if (!isdigit(*argv[i]))
+{
+printf("Error\n");
+exit(98);
+}
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[2]);
+
+result = num1 * num2;
+
+printf("%d\n", result);
+
+	return (0);
+}
+
