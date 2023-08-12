644d9603ae0cdb3c9bc35556063e146de3e1705b
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-08-11 18:06:53 -0700

    task2

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,33 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
+/**
+ * main - ..
+ * @argc: ...
+ * @argv: ...
+ * Return: ...
+ */
 
 int main(int argc, char *argv[])
 {
-int num1, num2, result;
-int i;
+	int num1, num2, result;
+	int i;
 
-if (argc != 3)
-{
-printf("Error\n");
-exit(98);
-}
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
 
-for (i = 1; i < argc; i++)
-{
-if (!isdigit(*argv[i]))
-{
-printf("Error\n");
-exit(98);
-}
-}
+	for (i = 1; i < argc; i++)
+	{
+		if (!isdigit(*argv[i]))
+		{
+		printf("Error\n");
+		exit(98);
+		}
+	}
 
-num1 = atoi(argv[1]);
-num2 = atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-result = num1 * num2;
+	result = num1 * num2;
 
-printf("%d\n", result);
+	printf("%d\n", result);
 
 	return (0);
 }
