1184d9eaf32c825ba09e6af9f31684d95faf8556
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-08-25 19:21:47 -0700

    task2

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,10 +4,18 @@
  * print_before_main - ...
  */
 
+#include <stdio.h>
+
+/* Define a function with the constructor attribute */
 void __attribute__((constructor)) print_before_main()
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+    printf("You're beat! and yet, you must allow,\n");
+    printf("I bore my house upon my back!\n");
 }
 
+int main(void)
+{
+    printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+    return 0;
+}
 
