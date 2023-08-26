1d8b2db7c4d76e0bc3635296be2b5ec33fe8db9b
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-08-25 19:18:56 -0700

    task2

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * print_before_main - ...
+ */
+
+void __attribute__((constructor)) print_before_main()
+{
+	printf("You're beat! and yet, you must allow,
+			\nI bore my house upon my back!\n");
+}
+
+
