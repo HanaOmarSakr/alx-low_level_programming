6ef2fc7c01edd50e6be1717797008c0b02874105
HanaOmarSakr <hana.omar.sakr@gmail.com>
2023-08-30 11:36:01 -0700

    task2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
-/*
+/**
  * struct dog - struct
  * @name: ..
  * @age: ...
  * @owner: ...
+ * Description: ...
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+}
 #endif
