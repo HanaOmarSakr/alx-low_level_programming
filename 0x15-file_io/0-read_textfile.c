ee2abc9e7b464572886f827ec51212a4a9a1aba5
Hana Sakr <hana.omar.sakr@gmail.com>
2023-09-07 00:44:44 +0300

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,19 +8,19 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    int fd;
-    ssize_t bytes;
-    char buffer[letters];
+	int fd;
+	ssize_t bytes;
+	char buffer[letters];
 
-    if (!filename || ! letters)
-        return (0);
+	if (!filename || ! letters)
+		return (0);
 
 	file = open(filename, O_RDONLY);
-    if (filename = -1)
-        return (0)
+	if (filename = -1)
+		return (0)
 
-    bytes = read(fd, &buffer[0]; letters);
-    bytes = write(STDOUT_FILENO, &buffer[0], letters);
-    close(fd);
-    return (letters);
+	bytes = read(fd, &buffer[0]; letters);
+	bytes = write(STDOUT_FILENO, &buffer[0], letters);
+	close(fd);
+	return (letters);
 }
