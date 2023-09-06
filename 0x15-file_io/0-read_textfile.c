434458356392bfcee77e48f45ddf2994083877ed
Hana Sakr <hana.omar.sakr@gmail.com>
2023-09-07 00:46:02 +0300

    push

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,17 +10,17 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	ssize_t bytes;
-	char buffer[letters];
+	char buffer[1024];
 
-	if (!filename || ! letters)
+	if (!filename || !letters)
 		return (0);
 
-	file = open(filename, O_RDONLY);
-	if (filename = -1)
-		return (0)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 
-	bytes = read(fd, &buffer[0]; letters);
+	bytes = read(fd, &buffer[0], letters);
 	bytes = write(STDOUT_FILENO, &buffer[0], letters);
 	close(fd);
-	return (letters);
+	return (bytes);
 }
