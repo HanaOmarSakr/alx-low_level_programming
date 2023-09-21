6f5bf04c21822e0cbc156aedbd0db62c49f39b42
Hana Sakr <hana.omar.sakr@gmail.com>
2023-09-21 15:02:54 +0300

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - .......
@@ -6,28 +7,26 @@
  * @letters: .....
  * Return: .........
 */
+
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int count = 0;
-	ssize_t bytes;
-	ssize_t bytes_read;
-	char buffer[1000000];
-
-	if (!filename || !letters)
-		return (0);
+	char *buffer;
+	ssize_t fd;
+	ssize_t s;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+		return (-1);
 
-	bytes = read(fd, , letters);
+	t = read(fd, buffer, letters);
+	s = write(STDOUT_FILENO, buffer, t);
 
-	while ((bytes_read = read(fd, &buffer[0], letters)) > 0) {
-        count += bytes_read;
-    }
-	if ()
-	bytes = write(STDOUT_FILENO, &buffer[0], letters);
+	free(buffer);
 	close(fd);
-	return (bytes);
+	return (s);
 }
